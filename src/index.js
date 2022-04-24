e9e8df362e0da71b334ef82072497c79479bbee2
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-04-23 20:49:36 -0400

    Completed assignment and in process rank and sort functions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 const form = document.getElementById('create-task-form');
 const taskDescription = document.getElementById('new-task-description');
 const newTaskUI = document.getElementById('tasks');
+let sortedTask = []
 let newTask 
 
 //listen for submit event
@@ -13,12 +14,15 @@ form.addEventListener('submit', function(event) {
 function createTask(event) {
   event.preventDefault(); //prevent form from redirecting 
   const newTaskDescription =  document.getElementById('new-task-description'); //get the new task description
-  appendTask(newTaskDescription) ; //call appendTask and add to to DO
+  const newTaskRankLow =  document.getElementById('new-task-priority-low'); //get the new task description
+  const newTaskRankMed =  document.getElementById('new-task-priority-med');
+  const newTaskRankHigh =  document.getElementById('new-task-priority-high');
+  sortedTask.push(newTaskDescription.value) //not operational build method .
+  appendTask(newTaskDescription, newTaskRankLow, newTaskRankMed, newTaskRankHigh) ; //call appendTask and add to to DO
   console.log(newTaskDescription);
-
 }
 
-function appendTask(task) {
+function appendTask(task, low, med, high) {
   newTask = newTaskUI.appendChild(document.createElement('li')).innerText = task.value
 
   console.log(`New task is:${task.value}`);
@@ -27,6 +31,27 @@ function appendTask(task) {
 }
 
 
+// A delete function that will remove tasks from your list
+function deleteTask() {
+  newTaskUI.remove()
+}
+
+// A priority value selected from a dropdown that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
+
+
+
+// As an additional challenge, implement a sorting functionality that displays the tasks in ascending or descending order based on priority
+
+
+
+// An additional input field (e.g. user, duration, date due)
+
+
+// Ability to edit tasks
+
+
+
+// Something of your choice! The main objective is to add a feature that allows the user's input to affect the DOM
 
 
 
